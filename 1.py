647c2ae56536ecca552a3b4cea275d751994592c
Charlie.Cao <pplboy@gmail.com>
2023-08-15 15:51:36 +0800

    init

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('给老顾做了一个Uber玩')
+st.title('Uber历史数据')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
